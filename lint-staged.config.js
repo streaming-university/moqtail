84437d3760d19877e97d706c23fe513c9ad84925
DenizUgur <DenizUgur@users.noreply.github.com>
2025-07-05 11:56:29 -0700

    chore: add husky for pre-commit checks

---
+++ lint-staged.config.js
@@ -0,0 +1,14 @@
+/**
+ * @filename: lint-staged.config.js
+ * @type {import('lint-staged').Configuration}
+ */
+export default {
+  // Run cargo fmt/clippy once for all Rust files
+  '(apps|libs)/**/*.{rs,toml}': () => ['cargo fmt --all', 'cargo clippy --all-targets --all-features -- -D warnings'],
+
+  // Run prettier for rest of the files
+  '(apps|libs)/**/*.ts': ['prettier --write'],
+  './*.{ts,js,json,md}': ['prettier --write'],
+  './.prettierrc': ['prettier --write'],
+  '.github/**/*': ['prettier --write']
+}
