f78d71b3b00705f2b0db5e0ea91b3da7ec07341e
Deniz Ugur <7467169+DenizUgur@users.noreply.github.com>
2025-07-06 09:14:13 -0700

    chore: expand lint staged to run build/test etc

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,13 +3,39 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  // Run cargo fmt/clippy once for all Rust files
-  '(apps|libs)/**/*.{rs,toml}': () => ['cargo fmt --all', 'cargo clippy --all-targets --all-features -- -D warnings'],
+  '(apps|libs)/**/*.{rs,toml}': (files) => {
+    const projects = files.reduce((acc, file) => {
+      const match = file.match(/^((apps|libs)\/[^/]+)/)
+      if (match) {
+        const project = match[1]
+        if (!acc.includes(project)) acc.push(project)
+      }
+      return acc
+    }, [])
+    return projects.flatMap((project) => {
+      const manifestPathArg = `--manifest-path=${project}/Cargo.toml`
+      const all = `--all-targets --all-features`
+      return [
+        // Run cargo clippy once for every changed project
+        `cargo clippy ${manifestPathArg} ${all} --fix --allow-dirty -- -D warnings`,
+        // Run cargo fmt once for every changed project
+        `cargo fmt ${manifestPathArg}`,
+        // Run cargo build once for every changed project
+        `cargo build ${manifestPathArg} ${all}`,
+        // Run cargo check once for every changed project
+        `cargo check ${manifestPathArg} ${all}`,
+        // Run cargo test once for every changed project
+        `cargo test ${manifestPathArg} ${all}`,
+      ]
+    })
+  },
+
+  // Run build/test for all changed projects (at once)
+  '(apps|libs)/**/*.ts': () => [
+    'npm run build', // The order of builds matter
+    'npm run test',
+  ],
 
   // Run prettier for rest of the files
-  '(apps|libs)/**/*.ts': ['prettier --write'],
-  './*.{ts,js,json,md}': ['prettier --write'],
-  './.prettierrc': ['prettier --write'],
-  '.github/**/*': ['prettier --write'],
-  '.changeset/*': ['prettier --write'],
+  '*': ['prettier --ignore-unknown --write'],
 }
