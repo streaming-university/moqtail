4a58ad68a15cc2190ed10ac9458bd3b05887aea0
DenizUgur <DenizUgur@users.noreply.github.com>
2025-08-24 15:25:55 -0700

    chore: separate moqtail-ts through entry points

--- libs/moqtail-ts/tsup.config.ts
+++ libs/moqtail-ts/tsup.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/index.ts', 'src/client/index.ts', 'src/model/index.ts', 'src/util/index.ts'],
   format: ['cjs', 'esm'],
   dts: true,
   outDir: 'dist',
   splitting: false,
   sourcemap: false,
   clean: true,
+  treeshake: true,
   define: {
     'import.meta.vitest': 'undefined',
   },
