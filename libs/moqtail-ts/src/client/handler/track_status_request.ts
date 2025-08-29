1bcb33d15e099935e27217e160dc4e38b79c3443
DenizUgur <DenizUgur@users.noreply.github.com>
2025-08-29 14:16:49 -0700

    fix: fix TrackStatusRequestMessage

--- libs/moqtail-ts/src/client/handler/track_status_request.ts
+++ libs/moqtail-ts/src/client/handler/track_status_request.ts
@@ -1,6 +1,6 @@
-import { TrackStatusRequest } from '../../model/control'
+import { TrackStatusRequestMessage } from '../../model/control'
 import { ControlMessageHandler } from './handler'
 
-export const handlerTrackStatusRequest: ControlMessageHandler<TrackStatusRequest> = async (_client, _msg) => {
+export const handlerTrackStatusRequest: ControlMessageHandler<TrackStatusRequestMessage> = async (_client, _msg) => {
   // TODO: Implement TrackStatusRequest handler logic
 }
